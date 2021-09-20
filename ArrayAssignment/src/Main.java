003c6e70ecfe86fb84c7075585b93b496aa89314
Tyler Gennaro <71794383+TGennnaro@users.noreply.github.com>
2021-09-20 10:18:07 -0400

    ArrayAssignment1

---
+++ ArrayAssignment/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+	public static void main(String[] args) {
+		double[] values = {25.3, 100, -10, -1.5, 13, 98.6, 17, 123.145, 125.6, 123.146};
+		
+		double largest = values[0];
+		int negatives = 0;
+		
+		for (int i=0;i<values.length;i++) {
+			System.out.print(values[i]+" ");
+			
+			if (values[i] > largest) {
+				largest = values[i];
+			}
+			if (values[i] < 0) {
+				negatives++;
+			}
+		}
+		System.out.println();
+		System.out.println("The maximum value in the array is "+largest+".");
+		System.out.println("There are a total of "+negatives+" negative values in the array.");
+	}
+
+}
