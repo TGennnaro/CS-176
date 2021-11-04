8648e91f727045649e5bcec1270d83dd389e6797
Tyler Gennaro <71794383+TGennnaro@users.noreply.github.com>
2021-11-04 11:17:28 -0400

    Add files via upload

--- Chpt10_Test/src/MeasurableTester.java
+++ Chpt10_Test/src/MeasurableTester.java
@@ -12,14 +12,19 @@ public class MeasurableTester
 	  double avgBal = Data.average(accounts);
 	  double maxBal = Data.max(accounts);
 	  System.out.println("Average balance: "+avgBal);
+	  System.out.println("Expected: 1000");
 	  System.out.println("Max balance: "+maxBal);
+	  System.out.println("Expected: 1500");
 	  
       Measurable[] countries = new Measurable[3];
       countries[0] = new Country("Uruguay", 176220);
       countries[1] = new Country("Thailand", 513120);
       countries[2] = new Country("Belgium", 30510);
 
+      double avgCountry = Data.average(countries);
       double maxCountry = Data.max(countries);
+      System.out.println("Average area: "+avgCountry);
+      System.out.println("Expected: 239950");
       System.out.println("Maximum area: " + maxCountry);
       System.out.println("Expected: 513120");
    }
